ea37cb8e1a3f9d1865d2f664a5ad0d43facf360d
Alex <alexcouts18@gmail.com>
2020-03-07 13:02:43 -0500

    aadded child component JSX markup

--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -1,3 +1,28 @@
 import React from "react";
+// import Appearances from ""
 
-export default ParentContainer
+const CharacterCard = () => { 
+
+
+
+    return (
+
+        <div>
+            <h1>{`Name:`}</h1>
+            <h3>{`Created:`}</h3>
+            <h3>{`Birth Year:`}</h3>
+            <h3>{`Home World:`}</h3>
+            <p>{`Mass:`}</p>
+            <p>{`Height:`}</p>
+            <p>{`Skin Colour:`}</p>
+            <p>{`Hair Color:`}</p>
+            <p>{`Gender:`}</p>
+            {/* <Appearances /> */}
+        </div>
+    )
+
+}
+
+
+
+export default CharacterCard
